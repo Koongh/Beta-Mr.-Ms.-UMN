3ad9c9224f84076841a2d4354be37e2544ed3740
KoongHiap <yapkng@gmail.com>
2023-04-09 22:27:10 +0700

    add: final form-regis

--- pages/form-regis.tsx
+++ pages/form-regis.tsx
@@ -2,7 +2,10 @@ import { useForm } from "react-hook-form";
 
 const FormRegis = () => {
   const { register, handleSubmit } = useForm();
-  const APP_URL = process.env.NODE_ENV == 'production' ? process.env.APP_URL : 'http://localhost:3000';
+  const APP_URL =
+    process.env.NODE_ENV == "production"
+      ? process.env.APP_URL
+      : "http://localhost:3000";
 
   const onSubmit = async (data: any) => {
     const entries = Object.entries(data);
@@ -16,20 +19,41 @@ const FormRegis = () => {
         formData.append(item[0], item[1][0]);
       }
     });
-	  
+
     const response = await fetch("/api/mahesa-registration", {
       method: "POST",
       body: formData,
     });
 
     const fetchData = await response.json();
-	 console.log(fetchData)
+    console.log(fetchData);
 
     if (fetchData.code == 200) {
       console.log("berhasil masuk");
+      let sendData = {
+        id: fetchData.data.StudentID,
+        name: fetchData.data.Name,
+        profile_picture: fetchData.data.picture,
+        cv: fetchData.data.cv,
+      };
+
+      const entries2 = Object.entries(sendData);
+      const formData2 = new FormData();
+      entries2.map((item: any) => {
+        formData2.append(item[0], item[1]);
+      });
+
+      await fetch(
+        "https://script.google.com/macros/s/AKfycbzrgOxoPx0cQ-I-K27hpGHYpaC7scC2GqcS1i0P-j99ZgIPhWxBIAiWZFpGnCQnqEw/exec",
+        {
+          method: "POST",
+          body: formData2,
+        }
+      );
+    } else {
+      console.log("gagal");
       return;
     }
-    console.log("gagal");
   };
 
   return (
@@ -44,7 +68,6 @@ const FormRegis = () => {
         </button>
       </form>
     </div>
-
   );
 };
 
